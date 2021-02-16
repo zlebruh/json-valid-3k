33a303ee5271569b16afae2b8c71a97a3983826c
zlebruh <code@battlebroyale.com>
2021-02-16 13:15:11 +0200

    - Changed: deep evaluation no longer stops during certain conditions. As a result, the entire schema is being processed, giving you the full picture. - Changed: internal structure no longer relies on a class with static methods - Improved JSDoc annotations

--- README.md
+++ README.md
@@ -17,7 +17,9 @@ A simple tool for validating complex JSON structures
 
 ## Usage - simple
 ```javascript
-const Validator = require('json-valid-3k');
+import { validate } from 'json-valid-3k';
+// OR
+// const { validate } = require('json-valid-3k');;
 
 const CFG_SIMPLE = {
   arr: [],                            // 01. Array
@@ -46,16 +48,42 @@ const SCHEMA_SIMPLE = {
   def: { type: null },
 };
 
-const test = Validator.validate(CFG_SIMPLE, SCHEMA_SIMPLE);
+const test = validate(CFG_SIMPLE, SCHEMA_SIMPLE);
 // test = {
 //   valid: true, // OR false
 //   tree: { YOUR_OBJECT_PROPS / ERRORS }, 
 // }
 ```
 
+## Validation object
+The validation object has a fairly simple structrure
+```javascript
+{
+  valid: Boolean,
+  tree: Object,
+}
+```
+1. `valid`: indicates your object's integrity with the schema.
+2. `tree`: this object represents your entire validated tree, as **described in the schema**.
+
+### **Important note about `tree`**
+The `tree` object discards everything from the source object that is not in the schema. The idea is that if you really care about your data integrity, than there's not reason not to use it as opposed to dragging everything esle from your source object.
+
+<br /><hr /><br />
+## Additional Schema Options
+```javascript
+{
+  // ...Some props
+  optional: Boolean,
+  default: 'Something', // Has to match the type. NOT applied when "optional" is "true"
+}
+```
+
+<br /><hr /><br />
+
 ## Usage - deep nesting
 ```javascript
-const Validator = require('json-valid-3k');
+import { validate } from 'json-valid-3k';
 
 const a_data = {
   a: 'kisk',
@@ -66,8 +94,16 @@ const a_data = {
       q: [
         { qub: 456 },
         { qub: 789 },
+        { qub: 'I am wrong, hehe' },
+        null,
+        undefined,
+        { irrelevant: 'prop' },
+        { qub: 111 },
       ]
-    }
+    },
+    yy: {
+      hello: 'World',
+    },
   },
   c: ['Dingo'],
   d: () => ({}),
@@ -88,40 +124,62 @@ const a_schema = {
             type: 'Array',
             children: {
               type: 'Object',
-              props: { qub: { type: 'Number' } }
-            }
-          }
-        }
-      }
+              props: { qub: { type: 'Number' } },
+            },
+          },
+        },
+      },
+      yy: {
+        type: 'Object',
+        props: {
+          hello: { type: 'String' },
+          aaa: { type: 'Boolean', default: false },
+          bbb: { type: 'Boolean', default: true },
+          ccc: { type: null, default: null },
+          ddd: { type: null, default: undefined },
+          eee: { type: 'String', default: 'This will show up', },
+          fff: { type: 'String', default: 'This will NOT show up because it is optional', optional: true },
+        },
+      },
     },
   },
   d: { type: 'Function' }
 };
 
-const test = Validator.validate(a_data, a_schema);
-// test = {
-//   valid: false, // OR true
-//   tree: {
-//     a: "kisk"
-//     b:
-//     bb: 123
-//     duck: "ERROR: Value [123] does not match type [String]"
-//     xx: {q: Array(2)}
-//     c: ["Dingo"]
-//     d: () => ({})
-//   }, 
-// }
-```
-
-## Additional Schema Options
-```javascript
-{
-  // ...Some props
-  optional: Boolean, // Overrides default value, if any
-  default: 'Something', // This could be literally anything
-  // The value is only assigned if the property is missing in the data structure
+const test = validate(a_data, a_schema);
+test = {
+  valid: false, // OR true
+  tree: {
+    a: "kisk"
+    b:
+    bb: 123
+    duck: "ERROR: Value [123] does not match type [String]"
+    xx: {
+      q: Array(7) [
+        0: {qub: 456}
+        1: {qub: 789}
+        2: {qub: "ERROR: Value ["I am wrong, hehe"] does not match type [Number]"}
+        3: "ERROR: Value [null] does not match type [Object]"
+        4: "ERROR: Value [undefined] does not match type [Object]"
+        5: {qub: "ERROR: Value [undefined] does not match type [Number]"}
+        6: {qub: 111}
+      ]
+    }
+    yy: {
+      aaa: false
+      bbb: true
+      ccc: null
+      ddd: "ERROR: Value [undefined] does not match type [null]"
+      eee: "This will show up"
+      hello: "World"
+    }
+    c: ["Dingo"]
+    d: () => ({})
+  }, 
 }
 ```
 
 ## Important
-You can used the **valid** property to determine wheter everythings fine but you can also dig deep in the structure to determine what exactly went wrong.
+Use the **`valid`** property to determine your object's integrity with the schema.
+
+The **`tree`** object stores your output. It contains your validates properties OR the relevant errors for each and every problem, as described in the scema.
--- index.js
+++ index.js
@@ -1,120 +1,133 @@
 const Tools = require('zletools');
 
-class Validator {
-  /**
-   * @param {object} cfg
-   * @param {object} schema
-   * @returns {boolean}
-   */
-  static validate(cfg, schema) {
-    Tools.checkForNumberOfArguments(2, [cfg, schema]);
-
-    if (!Tools.isObject(cfg, true)) {
-      throw new Error(Tools.textNotObject(cfg, 'cfg'));
-    }
-    if (!Tools.isObject(schema, true)) {
-      throw new Error(Tools.textNotObject(schema, 'schema'));
-    }
-
-    return Validator.doBranch(cfg, schema);
-  }
-
-  static typeError(value, type) {
-    return `ERROR: Value [${JSON.stringify(value)}] does not match type [${type}]`;
-  }
-
-  static checkArray(arr, children) {
-    const clone = [];
-    const { type, props } = children;
-    let valid = true;
-
-    for (let i = 0; i < arr.length; i += 1) {
-      const item = arr[i];
-      const typeMatch = Tools.getType(item) === type;
-
-      if (typeMatch) {
-        if (props) {
-          const branch = Validator.doBranch(item, props);
-          if (!branch.valid) {
-            valid = false;
-          }
-          clone.push(branch.tree);
-        } else {
-          clone.push(item);
-        }
-      } else {
-        valid = false;
-        clone.push(Validator.typeError(item, type));
-        break;
-      }
-    }
-
-    return {
-      valid,
-      arr: clone,
-    };
-  }
-
-  static doBranch(branch, rules = {}) {
-    const tree = {};
-    const keys = Object.keys(rules);
-    let valid = true;
-
-    // if (!keys.length) {
-    //   return {
-    //     tree: 'ERROR: There are no rules for this property',
-    //     valid: false,
-    //   };
-    // }
-
-    for (let i = 0; i < keys.length; i += 1) {
-      const key = keys[i];
-      const rule = rules[key];
-      const { type, children } = rule;
-      let item = branch[key];
-
-      if (!Tools.is(item)) {
-        if (rule.optional === true) continue;
-
-        if (Tools.is(rule.default)) {
-          item = rule.default;
-        }
-      }
-
-      const sameType = Validator.matchType(item, type);
-      if (sameType) {
-        if (type === 'Object') {
-          const subBranch = Validator.doBranch(item, rule.props);
-          if (!subBranch.valid) {
-            valid = false;
-          }
-          tree[key] = subBranch.tree;
-        } else if (type === 'Array' && children) {
-          const checke = Validator.checkArray(item, children);
-          if (!checke.valid) {
-            valid = false;
-          }
-          tree[key] = checke.arr;
-        } else {
-          tree[key] = item;
-        }
-      } else {
-        tree[key] = Validator.typeError(item, type);
-        valid = false;
-      }
-    }
-
-    return { valid, tree };
-  }
-
-  static matchType(value, type, checkEmpty = false) {
-    if (value === type) return true;
-
-    const method = Tools[`is${type}`];
-    return method
-      ? method(value, checkEmpty)
-      : Tools.capitalize(typeof value) === type;
-  }
+// Local
+/**
+ * @param {*} value
+ * @param {String} type
+ * @returns {String}
+ */
+function typeError(value, type) {
+  return `ERROR: Value [${JSON.stringify(value)}] does not match type [${type}]`;
 }
 
-exports.validate = Validator.validate;
+/**
+ * @param {Array} arr
+ * @param {String} children
+ * @returns {{valid: Boolean, arr: Array}}
+ */
+function checkArray(arr, children) {
+  const clone = [];
+  const { type, props } = children;
+  let valid = true;
+
+  for (let i = 0; i < arr.length; i += 1) {
+    const item = arr[i];
+    const typeMatch = Tools.getType(item) === type;
+
+    if (typeMatch) {
+      if (props) {
+        const branch = doBranch(item, props);
+        if (!branch.valid) {
+          valid = false;
+        }
+        clone.push(branch.tree);
+      } else {
+        clone.push(item);
+      }
+    } else {
+      valid = false;
+      clone.push(typeError(item, type));
+    }
+  }
+
+  return {
+    valid,
+    arr: clone,
+  };
+}
+
+/**
+ * @param {Object} branch
+ * @param {Object} rules
+ * @returns {{valid: Boolean, tree: Object}}
+ */
+function doBranch(branch, rules = {}) {
+  const tree = {};
+  const keys = Object.keys(rules);
+  let valid = true;
+
+  for (let i = 0; i < keys.length; i += 1) {
+    const key = keys[i];
+    const rule = rules[key];
+    const { type, children } = rule;
+    let item = branch[key];
+
+    if (!Tools.is(item)) {
+      if (rule.optional === true) continue;
+
+      if (rule.hasOwnProperty('default')) {
+        item = rule.default;
+      }
+    }
+
+    const sameType = matchType(item, type);
+    if (sameType) {
+      if (type === 'Object') {
+        const subBranch = doBranch(item, rule.props);
+        if (!subBranch.valid) {
+          valid = false;
+        }
+        tree[key] = subBranch.tree;
+      } else if (type === 'Array' && children) {
+        const checke = checkArray(item, children);
+        if (!checke.valid) {
+          valid = false;
+        }
+        tree[key] = checke.arr;
+      } else {
+        tree[key] = item;
+      }
+    } else {
+      tree[key] = typeError(item, type);
+      valid = false;
+    }
+  }
+
+  return { valid, tree };
+}
+
+/**
+ * @param {*} value
+ * @param {String} type
+ * @param {Boolean} [checkEmpty]
+ * @return {Boolean}
+ */
+function matchType(value, type, checkEmpty = false) {
+  if (value === type) return true;
+
+  const method = Tools[`is${type}`];
+  return method
+    ? method(value, checkEmpty)
+    : Tools.capitalize(typeof value) === type;
+}
+
+// Exported
+/**
+ * @param {Object} cfg
+ * @param {Object} schema
+ * @returns {{valid: boolean, tree: Object}}
+ */
+function validate(cfg, schema) {
+  Tools.checkForNumberOfArguments(2, [cfg, schema]);
+
+  if (!Tools.isObject(cfg, true)) {
+    throw new Error(Tools.textNotObject(cfg, 'cfg'));
+  }
+  if (!Tools.isObject(schema, true)) {
+    throw new Error(Tools.textNotObject(schema, 'schema'));
+  }
+
+  return doBranch(cfg, schema);
+}
+
+exports.validate = validate;
